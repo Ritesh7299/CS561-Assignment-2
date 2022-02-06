17470a53e8e426bf99a3d5e8f5d6bce9e0c54460
Chaudhari Riteshkumar <57784297+Ritesh7299@users.noreply.github.com>
2022-02-06 15:39:10 -0800

    Update node.js

--- node.js
+++ node.js
@@ -1,16 +1,57 @@
-const express = require('express')
-const app = express()
-const port = 3000
+" use strict"
+var express = require('express')
+var app = express()
+var cors = require('cors')
+let token = ["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"]
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
 
-app.get('/data/2.5/weather', get_weather)
+app.get('/v1/weather',get_weather_v1)
+app.get('/v1/hello',get_hello)
+app.post('/v1/auth',post_auth)
 
-function get_weather(req,response)
-{
-    response.json
-    ({"coord":{"lon":-123.262,"lat":44.5646},"weather":[{"id":701,"main":"Mist","description":"mist","icon":"50n"}],"base":"stations","main":{"temp":276.8,"feels_like":276.8,"temp_min":274.72,"temp_max":279.37,"pressure":1024,"humidity":79},"visibility":3219,"wind":{"speed":0,"deg":0},"clouds":{"all":100},"dt":1642314757,"sys":{"type":2,"id":2012991,"country":"US","sunrise":1642261568,"sunset":1642294710},"timezone":-28800,"id":5720727,"name":"Corvallis","cod":200})
+let jsonStr = JSON.stringify({'now': new Date()}) ;   
+//console.log(jsonStr);
+
+function get_weather_v1(req, res) {
+   
+        
+        res.json({"coord":{"lon":-123.262,"lat":44.5646},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],"base":"stations","main":{"temp":282.61,"feels_like":282.61,"temp_min":280.58,"temp_max":285.29,"pressure":1018,"humidity":84},"visibility":10000,"wind":{"speed":0.89,"deg":225,"gust":0.89},"clouds":{"all":0},"dt":1642038331,"sys":{"type":2,"id":2040223,"country":"US","sunrise":1642002454,"sunset":1642035291},"timezone":-28800,"id":5720727,"name":"Corvallis","cod":200})
+        
+    
 }
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:$\{port\}`)
-})
+function get_hello(req,res){
+   
+    res.json({"hello": "Hey there! Hope you are hving a great day"})
+    
+}
+// const fs = require("fs");
+
+function post_auth(req,res){
+    let usernames = ['Ritesh','Kalp','Yash']
+    let passwords = ['123','abc','xyz']
+    let username = req.body.username
+    let pwd = req.body.password
+   
+
+    if(usernames.includes(username)){
+      if(passwords.includes(pwd)){
+          res.json({ "Sucess" : "Please use the resources responsibly!","access-token": token,
+          "expires": "2022-02-20T22:25:42.142Z"})
+          
+      }
+  }
+
+
+
+}
+
+app.listen(3000)
+console.log('Node.js Express server is running on port 3000')
+    
+   
